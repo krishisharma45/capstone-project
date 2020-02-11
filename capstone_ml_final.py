53196525e94d606614dfe14b34d0c54c6f3c6070
Krishi Sharma <54914189+krishisharma45@users.noreply.github.com>
2020-02-10 19:17:59 -0800

    Add Decision Tree model and pickle for later use

---
+++ capstone_ml_final.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+#Load and split the data
+training_data = pd.read_csv("train.csv")
+testing_data = pd.read_csv("test.csv")
+costa_rica_data = training_data.drop(['Target'], axis=1)
+costa_rica_target = training_data['Target']
+
+#Clean the data to either replace or remove string columns
+costa_rica_data.select_dtypes(exclude=[np.number]).head()
+costa_rica_data = costa_rica_data.select_dtypes(include=[np.number], exclude=[np.object]).fillna(0)
+
+#Split data into 80% train, 20% validation split
+X_train, X_test, y_train, y_test = train_test_split(costa_rica_data.values, costa_rica_target.values, test_size= 0.2, random_state=42)
+
+#Run Feature Importance to extract relevant features
+features = model.feature_importances_
+features_dict = dict(zip(costa_rica_data.columns.values, features))
+ranked_features_df = pd.DataFrame(features_dict,
+                                  index=['Feature Importance']).T.sort_values('Feature Importance', ascending=False)[:10]
+
+#Fit Decision Tree with these most important features
+X_train, X_test, y_train, y_test = train_test_split(costa_rica_data[ranked_features_df.index].values, costa_rica_target.values, test_size= 0.2, random_state=42)
+clf = DecisionTreeClassifier(criterion='gini', max_depth=60)
+model = clf.fit(X_train, y_train)
+train_score = model.score(X_train, y_train)
+val_score = model.score(X_test, y_test)
+print('Decision Tree Train Accuracy: '+str(round(train_score*100,2))+'%')
+print('Decision Tree Validation Accuracy: '+str(round(val_score*100,2))+'%')
+
+#Save the model to disk
+filename = 'capstone_ml_final.sav'
+pickle.dump(model, open, (filename, 'wb'))
+
+#Load the model from the disk
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, y_test)
+print(result)
