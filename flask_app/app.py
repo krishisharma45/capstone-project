87f0cd2b3f0a5be61c51a5276f44212a72f07fdf
Krishi Sharma <54914189+krishisharma45@users.noreply.github.com>
2020-03-06 15:45:45 -0500

    Add error handling (nulls, negative values, alpha)

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
 import os
+import sys
+
+print('This is standard output', file=sys.stdout)
 
 app = Flask(__name__)
 
@@ -13,6 +15,20 @@ def model(input_list):
     result = loaded_model.predict(input)
     return result[0]
 
+def validate_fields(input_list):
+    for value in input_list:
+        if value is '':
+            return False
+        if value.isalpha():
+            return False
+    return True
+
+def validate_values(input_list):
+    for value in input_list:
+        if value < 0:
+            return False
+    return True
+
 @app.route('/')
 def display_form():
     return render_template('index.html')
@@ -22,18 +38,24 @@ def display_prediction():
     if request.method == 'POST':
         input_list = request.form.to_dict()
         input_list = list(input_list.values())
-        input_list = list(map(float, input_list))
-        result = model(input_list)
-        if int(result) == 1:
-            prediction = 'Extremely Vulnerable'
-        elif int(result) == 2:
-            prediction = 'Highly Vulnerable'
-        elif int(result) == 3:
-            prediction = 'Somewhat Vulnerable'
+        if validate_fields(input_list) == False:
+            return render_template('error.html')
         else:
-            prediction = 'Not Vulnerable'
-        return render_template('results.html', prediction = prediction)
+            input_list = list(map(float, input_list))
+            if validate_values(input_list) == False:
+                return render_template('error.html')
+            else:
+                result = model(input_list)
+                if int(result) == 1:
+                    prediction = 'Extremely Vulnerable'
+                elif int(result) == 2:
+                    prediction = 'Highly Vulnerable'
+                elif int(result) == 3:
+                    prediction = 'Somewhat Vulnerable'
+                else:
+                    prediction = 'Not Vulnerable'
+                return render_template('results.html', prediction = prediction)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
